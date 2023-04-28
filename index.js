7e8e7f007296159a3b02ec4e575cbf67932db005
Wilder <wkerubo28@gmail.com>
2023-04-28 17:30:06 +0300

    Completed Project

--- index.js
+++ index.js
@@ -41,22 +41,31 @@ window.addEventListener('load', handleResponsiveShopNowButton);
 window.addEventListener('resize', handleResponsiveShopNowButton);
 
 
-function handleLikeClick(event) {
-  event.preventDefault();
-  const likeBtn = event.target;
-  const likeCount = likeBtn.nextElementSibling;
-  const currentCount = parseInt(likeCount.textContent);
-  likeCount.textContent = currentCount + 1;
-}
+// function handleLike() {
+//   let numb = parseInt(document.getElementById("count").innerHTML);
+//   parseIntdocument.getElementById("count").innerHTML = numb + 1;
 
-const cards = document.querySelectorAll('.card1, .card2, .card3, .card4');
-cards.forEach((card) => {
-  const img = card.querySelector('img');
-  const likeBtn = document.createElement('a');
-  likeBtn.textContent = 'Like';
-  likeBtn.classList.add('like-btn');
-  likeBtn.addEventListener('click', handleLikeClick);
-  const likeCount = document.createElement('span');
-  likeCount.textContent = '0';
-  img.after(likeBtn, likeCount);
-});
+  function handleLike() {
+    let numb = parseInt(document.getElementById("count").innerHTML);
+    document.getElementById("count").innerHTML = numb + 1;
+    alert(document.getElementById("count").innerHTML );
+    
+  }
+  
+  // const likeBtn = event.target;
+  // const likeCount = likeBtn.nextElementSibling;
+  // const currentCount = parseInt(likeCount.textContent);
+  // likeCount.textContent = currentCount + 1;
+
+
+// const cards = document.querySelectorAll('.card1, .card2, .card3, .card4');
+// cards.forEach((card) => {
+//   const img = card.querySelector('img');
+//   const likeBtn = document.createElement('a');
+//   likeBtn.textContent = 'Like';
+//   likeBtn.classList.add('like-btn');
+//   likeBtn.addEventListener('click', handleLikeClick);
+//   const likeCount = document.createElement('span');
+//   likeCount.textContent = '0';
+//   img.after(likeBtn, likeCount);
+// });
