a03645d085032447ffcbb1751c701cba6d8f6df7
Wilder Ondieki <126570212+wondieki@users.noreply.github.com>
2023-05-02 23:21:04 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,49 +1,33 @@
-//https://my-json-server.typicode.com/wondieki/project
-
-function search() {
-  // get input value
-  const input = document.getElementById("search-bar").value.toLowerCase();
-
-  // get all elements to search
-  const elements = document.querySelectorAll(".searchable");
-
-  // loop through elements and hide/show based on search term
-  elements.forEach(function(element) {
-    if (element.textContent.toLowerCase().indexOf(input) > -1) {
-      element.style.display = "";
-    } else {
-      element.style.display = "none";
-    }
-  });
-}
+// //https://my-json-server.typicode.com/wondieki/project
 
 
 
+fetch('https://my-json-server.typicode.com/wondieki/project')
+
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
+document.addEventListener("DOMContentLoaded", function(){
+    const url = "http://localhost:3000/perfumes/"
+})
+
+const perfumeTitles = document.querySelectorAll('.perfume-title');
+
+perfumeTitles.forEach((title) => {
+    title.addEventListener('click', (event) => {
+        // handle the click event
+    });
+});
 
 
-// Get the button element
-const btnShopNow = document.querySelector('.btn-shop-now');
+const perfumeDescription = document.querySelectorAll('.perfume-description');
 
-// Define the screen size threshold
-const screenSizeThreshold = 768; // Example: 768 pixels
-
-// Add a class to the button element when the screen size is below the threshold
-function handleResponsiveShopNowButton() {
-  if (window.innerWidth < screenSizeThreshold) {
-    btnShopNow.classList.add('btn-shop-now--responsive');
-  } else {
-    btnShopNow.classList.remove('btn-shop-now--responsive');
-  }
-}
-
-// Call the function on page load and on window resize
-window.addEventListener('load', handleResponsiveShopNowButton);
-window.addEventListener('resize', handleResponsiveShopNowButton);
-
-
-// function handleLike() {
-//   let numb = parseInt(document.getElementById("count").innerHTML);
-//   parseIntdocument.getElementById("count").innerHTML = numb + 1;
+perfumeDescription.forEach(() => {
+    description.addEventListener('mouseover', (event) => {
+        // handle the mouseover event
+    });
+});
 
   function handleLike() {
     let numb = parseInt(document.getElementById("count").innerHTML);
@@ -51,21 +35,4 @@ window.addEventListener('resize', handleResponsiveShopNowButton);
     alert(document.getElementById("count").innerHTML );
     
   }
-  
-  // const likeBtn = event.target;
-  // const likeCount = likeBtn.nextElementSibling;
-  // const currentCount = parseInt(likeCount.textContent);
-  // likeCount.textContent = currentCount + 1;
 
-
-// const cards = document.querySelectorAll('.card1, .card2, .card3, .card4');
-// cards.forEach((card) => {
-//   const img = card.querySelector('img');
-//   const likeBtn = document.createElement('a');
-//   likeBtn.textContent = 'Like';
-//   likeBtn.classList.add('like-btn');
-//   likeBtn.addEventListener('click', handleLikeClick);
-//   const likeCount = document.createElement('span');
-//   likeCount.textContent = '0';
-//   img.after(likeBtn, likeCount);
-// });
